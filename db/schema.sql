33060a2108b990838bcfa811d2e0e52c307db69a
Emerson Recto <emersontrecto@gmail.com>
2025-06-30 16:05:28 -0400

    workshop

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,18 @@
 -- TODO
+create table folders {
+  id serial [pk]
+  name text [unique, not null]
+}
+
+create table files {
+  id serial [pk]
+  name text [not null]
+  size int [not null]
+  folder_id int [not null]
+
+  indexes {
+    (name, folder_id) [unique]
+  }
+}
+
+Ref: folders.id < files.folder_id
